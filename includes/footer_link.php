9c2f34468da3840312efd0bafd5b38e5f0ebc571
⚡  HARISH ⚡ <harishpr546@@gmail.com>
2025-10-06 18:43:25 +0530

    Cart Vraiant Understanding

--- includes/footer_link.php
+++ includes/footer_link.php
@@ -1,7 +1,7 @@
     <!--JS File Include -->
     <script src="<?= APP_URL ?>themes/theme9/js/script.js"></script>
     <script src="<?= APP_URL ?>themes/theme7/common.js"></script>
-    <script src="<?= APP_URL ?>themes/theme7/ajax/common-cart.js"></script>
+    <script src="<?= APP_URL ?>themes/theme9/ajax/common-cart.js"></script>
 
     <script src="<?= APP_URL ?>shop/javascripts/common-wishlist.js"></script>
     <script src="<?= APP_URL ?>assets/js/global.js"></script>
